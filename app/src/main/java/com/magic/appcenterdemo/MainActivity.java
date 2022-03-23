721727ecfe2a4ca4b8cc2164a91e8597c610adb5
MAGICSW\abhishek.nigam <abhishek.nigam@magicedtech.com>
2022-03-23 14:57:58 +0530

    First push to App Center Demo

---
+++ app/src/main/java/com/magic/appcenterdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.magic.appcenterdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "e6d2aae0-ac57-42ba-a841-0052ddf79398", Analytics.class, Crashes.class);
+    }
+}
